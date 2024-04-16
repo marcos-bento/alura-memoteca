9cdade4ee789c5b457700d49669201233c7d9bd2
Marcos Bento <marcos.x.marcos@hotmail.com>
2024-04-16 19:11:33 -0300

    Finalização da aula 2

---
+++ src/app/componentes/pensamentos/criar-pensamento/criar-pensamento.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-criar-pensamento',
+  templateUrl: './criar-pensamento.component.html',
+  styleUrls: ['./criar-pensamento.component.css']
+})
+export class CriarPensamentoComponent implements OnInit {
+
+  criarPensamento(){
+    event?.preventDefault();
+    alert("Evento adicionado!");
+  }
+
+  cancelarPensamento(){
+    event?.preventDefault();
+    alert("Pensamento cancelado!");
+  }
+
+  pensamento ={
+    id: '1',
+    conteudo: 'Aprendendo Angular',
+    autoria: 'Marcos Bento',
+    modelo: 'modelo1'
+  }
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
