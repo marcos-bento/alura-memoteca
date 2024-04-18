253189163b44d5ebfc4b7e9d96d08cad3f8b224d
Marcos Bento <marcos.x.marcos@hotmail.com>
2024-04-18 15:22:22 -0300

    Finalização da aula 4

--- src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
+++ src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
@@ -7,6 +7,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListarPensamentoComponent implements OnInit {
 
+  listaPensamentos = [
+    { conteudo: 'I love Angular', autoria: 'Max', modelo: "modelo3"}
+  ];
+
   constructor() { }
 
   ngOnInit(): void {
